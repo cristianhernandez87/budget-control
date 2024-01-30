b1aef207978e255e684f4014dc114fb47945c213
Cristian Hernandez <cristiancamilo.hernandezr@hotmail.com>
2024-01-30 16:33:11 -0500

    ok budget and prices 2

--- budget-product/src/components/Expend.jsx
+++ budget-product/src/components/Expend.jsx
@@ -1,16 +1,39 @@
+import {
+    LeadingActions,
+    SwipeableList, 
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions
+} from 'react-swipeable-list'
+import 'react-swipeable-list/dist/styles.css'
+
 import { getDate  } from '../helpers'
 
 function Expend({expend}) {
     const { name, amount, category, id, fecha}  = expend
 
-    console.log(category)
+    const leadindActions = () => {
+        console.log('iz')
+    }
+
+    const trailActions = () => {
+        console.log('der')
+    }
+
     return (
-        <div className="card p-3 mb-3 w-100">
-            <p className="mb-0"><strong>Name:</strong> {name}</p>
-            <p className="mb-0"><strong>Amount:</strong> ${amount}</p>
-            <p className="mb-0"><strong>Category:</strong> {category}</p>
-            <p className="mb-0"><strong>Date:</strong> {getDate(fecha)}</p>
-        </div>
+        <SwipeableList>
+            <SwipeableListItem
+                leadindActions={leadindActions}
+                trailActions={trailActions}
+            >
+                <div className="card p-3 mb-3 w-100">
+                    <p className="mb-0"><strong>Name:</strong> {name}</p>
+                    <p className="mb-0"><strong>Amount:</strong> ${amount}</p>
+                    <p className="mb-0"><strong>Category:</strong> {category}</p>
+                    <p className="mb-0"><strong>Date:</strong> {getDate(fecha)}</p>
+                </div>
+            </SwipeableListItem>
+        </SwipeableList>
     )
 }
 
